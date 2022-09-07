f805bdc36f0080c4db25dd0efbe1fb3fba71e061
Jenna Hendrix <JMHendrix@student.fullsail.edu>
2022-09-07 10:11:36 -0400

    Added bullet.cs and some changes to the bullet prefab.

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    [Header("----- Components -----")]
+    [SerializeField] Rigidbody rb;
+
+    [Header("----- Bullet Stats -----")]
+    public int damage;
+    public int speed;
+    public int destroyTime;  //this is for clean up, so the frames arent falling. taking up too much memory.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.forward * speed; // sends the bullet toward the transforms forward direction. 
+        Destroy(gameObject, destroyTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<IDamageable>() != null)
+        {
+            other.GetComponent<IDamageable>().takeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
