900b1a1ec22d679ed3b1d8e45cda62a0c086f050
Joshua Luvene <95545896+Lokheart12@users.noreply.github.com>
2022-09-07 17:26:11 -0400

    Added Buttons script
    I added the script for the buttons. I've commented out the sections in the script that don't work with the current build, but will uncomment them when the appropriate functionality is added

---
+++ Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/*This file creates the functions for all the buttons used within
+the menus. Some of the functions have been commented out to preserve
+the functionality of the code while other pieces are being uploaded:
+-line 24
+-line 30
+-line 36
+-line 47
+-line 52
+-line 57
+*/
+public class buttonFunctions : MonoBehaviour
+{
+    //for button that resumes the game
+    public void resume()
+    {
+        if (gameManager.instance.isPaused)
+        {
+            gameManager.instance.isPaused = !gameManager.instance.isPaused;
+           // gameManager.instance.cursorUnlockUnpause();
+        }
+    }
+
+    public void restart()
+    {
+       // gameManager.instance.cursorUnlockUnpause();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void playerRespawn()
+    {
+       // gameManager.instance.playerScript.respawn();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    //button to give player HP
+    public void giveHP(int amount)
+    {
+       // gameManager.instance.playerScript.giveHP(amount);
+    }
+
+    public void giveJump(int amount)
+    {
+       // gameManager.instance.playerScript.giveJump(amount);
+    }
+
+    public void giveSpeed(int amount)
+    {
+       // gameManager.instance.playerScript.giveSpeed(amount);
+    }
+}
