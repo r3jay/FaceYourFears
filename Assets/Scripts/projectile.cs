ee0c8734719642c8c8c14a35d38eb3637da7922d
raize27 <rj.rodriguezroman@gmail.com>
2022-10-07 22:58:00 -0400

    Player new model plus animations
    -Added 2 scripts. Will keep editing them to work on later for the projectiles
    -Created a folder to add projectiles so far
    -Modified the player and added a model plus animations (still needs work, created a prefab of such under player named "AnimPlayer")
    -Created a folder for downloads from Mixamo

---
+++ Assets/Scripts/projectile.cs
@@ -0,0 +1,17 @@
+//Will be using this code later to work on the projectiles - Ricardo
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class projectile : MonoBehaviour
+//{
+//    private bool collided;
+//    private void OnCollisionEnter(Collision collision)
+//    {
+//        if(collision.gameObject.tag != "Bullet" && collision.gameObject.tag != "Player" && !collided)
+//        {
+//            collided = true;
+//            Destroy(gameObject);
+//        }
+//    }
+//}
